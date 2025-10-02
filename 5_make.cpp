2fc9fbf02e6205b757ce9bb2b6f71528c4a8242a
MarkWehdeJr <wehde98@gmail.com>
2025-10-02 17:02:11 +0000

    Vector and Arrays Homework

--- 5_make.cpp
+++ 5_make.cpp
@@ -2,30 +2,42 @@
 #include <vector>
 #include <cstdlib>
 #include <string>
+#include <ctime> // for time()
+
 using namespace std;
 
 int main() {
-    //srand(123); // Seed the random number generator
+    // Seed the random number generator with the current time
+    srand(time(0));
+
+    // Vector to hold the coin flip results
+    vector<string> coinFlips;
+    string coinFlipResult = "";
 
     // Flip a coin 500 times
-    std::vector <std::string> coinFlips;
-    string coinFlipResult = "";
-    
-    // write a for loop that iternates over 500 flips and counts heads or tails in the coinFlips vector
-
+    for (int i = 0; i < 500; ++i) {
+        int thisRandomNumber = rand();
+        if ((thisRandomNumber % 2) == 0) {
+            coinFlipResult = "Heads";
+        } else {
+            coinFlipResult = "Tails";
+        }
+        coinFlips.push_back(coinFlipResult);
+    }
 
     // Count the number of heads
+    // const prevents the loop from modifying any of the "flips"
+    // auto tells the compiler to "figure out" which type of element it is using
+    // & prevents the loop from making temporary copies in the memory
     int headsCount = 0;
-
-    //handy code to iterate through a vector.  I'll give this to you for future reference.
-    //the auto& is a nice way to have c++ declare the variable as whatever type the variable that follows is first assigned to!  In this case it will be a String, because conflips is declared that way earlier
     for (const auto& flip : coinFlips) {  
         if (flip == "Heads") {
             ++headsCount;
         }
     }
-    std::cout << "Number of Heads: " <<  headsCount << '\n';
-    std::cout << "Number of Tails: " << coinFlips.size() - headsCount << '\n';
+
+    // Output results
+    cout << "Number of Heads: " << headsCount << '\n';
+    cout << "Number of Tails: " << coinFlips.size() - headsCount << '\n';
 
     return 0;
-}
